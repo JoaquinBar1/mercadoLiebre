25b89bbf5ca00fce17ed70cbe4ca76a6e0533972
joaquinBar1 <joa.barrionuevo@gmail.com>
2021-10-12 11:16:17 -0300

    formularios y estilos css

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
 }); 
+app.get('/registrarUsuario', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+}); 
+app.get('/logIn', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/logIn.html'))
+}); 
 
 
 app.listen (3030, () => console.log('Servidor Corriendo')  
